bb61435348722ce43a6f565af5a6474d00ed64be
morgan4563 <morganpark4563@gmail.com>
2025-07-11 12:12:07 +0900

    MovieViewController 파일 생성

---
+++ SeSAC7Week3/MovieViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MovieViewController.swift
+//  SeSAC7Week3
+//
+//  Created by hyunMac on 7/11/25.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
