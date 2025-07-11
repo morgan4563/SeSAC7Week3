bfe8728a3ba5d4b9c85531ce235062d615cbe78b
morgan4563 <morganpark4563@gmail.com>
2025-07-11 11:06:15 +0900

    Initial Commit

---
+++ SeSAC7Week3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC7Week3
+//
+//  Created by hyunMac on 7/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(1)
+        print(2)
+        print(3)
+    }
+}
+
